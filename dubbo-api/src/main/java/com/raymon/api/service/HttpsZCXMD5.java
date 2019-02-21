5c3c2df27fbeac9ad40e62989f6815744247a826
raymon <xingyunruyubb@gmail.com>
2019-02-21 13:40:43 +0800

    使用https get方式访问中诚信接口

--- dubbo-api/src/main/java/com/raymon/api/service/HttpsZCXMD5.java
+++ dubbo-api/src/main/java/com/raymon/api/service/HttpsZCXMD5.java
@@ -4,7 +4,6 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
