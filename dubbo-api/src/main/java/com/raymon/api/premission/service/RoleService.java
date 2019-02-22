6a937c3307736b4fcb60a93ae7176afe0e010618
weiguo.zhang <xingyunruyubb@gmail.com>
2019-02-22 11:20:13 +0800

    启动provider和consumer

---
+++ dubbo-api/src/main/java/com/raymon/api/premission/service/RoleService.java
@@ -0,0 +1,38 @@
+package com.raymon.api.premission.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.foryou.common.model.URole;
+import com.foryou.core.mybatis.page.Pagination;
+import com.foryou.premission.bo.RolePermissionAllocationBo;
+
+public interface RoleService {
+
+	int deleteByPrimaryKey(Long id);
+
+    int insert(URole record);
+
+    int insertSelective(URole record);
+
+    URole selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(URole record);
+
+    int updateByPrimaryKey(URole record);
+
+	Pagination<URole> findPage(Map<String, Object> resultMap, Integer pageNo,
+                               Integer pageSize);
+
+	Map<String, Object> deleteRoleById(String ids);
+
+	Pagination<RolePermissionAllocationBo> findRoleAndPermissionPage(
+            Map<String, Object> resultMap, Integer pageNo, Integer pageSize);
+	//根据用户ID查询角色（role），放入到Authorization里。
+	Set<String> findRoleByUserId(Long userId);
+
+	List<URole> findNowAllPermission();
+	//初始化数据
+	void initData();
+}
